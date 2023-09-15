00e81d9bb63e31e9f0454b3a2df1dacca235f69b
George Halterman <113994972+Agoews@users.noreply.github.com>
2023-09-14 22:00:47 -0700

    added error page, loading page, and additional routing

---
+++ app/loading.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Loading = async () => {
+  return <div className="loading loading-spinner loading-sm">...Loading</div>;
+};
+
+export default Loading;
--- app/users/new/page.tsx
+++ app/users/new/page.tsx
@@ -1,7 +1,18 @@
+"use client";
+
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const NewUsersPage = () => {
-  return <div>NewUsersPage</div>;
+  const router = useRouter();
+
+  return (
+    <div>
+      <button className="btn btn-primary" onClick={() => router.push("/users")}>
+        Create
+      </button>
+    </div>
+  );
 };
 
 export default NewUsersPage;
